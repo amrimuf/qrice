9ab44c2236598b7719d745780be0212ea59fa35f
Amri Mufti <amrimuvti@gmail.com>
2023-05-27 12:46:53 +0700

    add swagger docs

--- server.js
+++ server.js
@@ -8,8 +8,19 @@ const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const predictionRoutes = require('./routes/predictionRoutes');
 
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('js-yaml');
+const fs = require('fs');
+
 app.use(cors())
 app.use(express.json());
+
+const spec = yaml.load(fs.readFileSync('./swagger.yaml', 'utf-8'));
+const router = express.Router();
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(spec));
+app.use('/', router);
+
 
 // Routes
 app.use('/api', authRoutes);
