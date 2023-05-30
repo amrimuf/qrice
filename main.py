c4313a92b51ec0f2affeaa7c1fb7fe673e2e43a6
amrimuf <amrimuvti@gmail.com>
2023-05-30 04:23:23 +0000

    add other models

--- main.py
+++ main.py
@@ -11,36 +11,41 @@ from flask_cors import CORS
 bucket_name = 'env-prod'
 images_bucket_name = 'q-rice'
 
-model_filename = 'model-rice-diseases.h5'
-model_path = f'gs://{bucket_name}/{model_filename}'
-
-# Create a client for interacting with the GCS API
-storage_client = storage.Client.from_service_account_json('./service-account-key.json')
-
-# Create the directory if it doesn't exist
-os.makedirs('/tmp/', exist_ok=True)
-
-# Download the model file to a temporary location
-temp_model_path = f'/tmp/{model_filename}'
-model_blob = storage_client.get_bucket(bucket_name).blob(model_filename)
-model_blob.download_to_filename(temp_model_path)
-
-# Load the model from the H5 file
-model = keras.models.load_model(temp_model_path, compile=False)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
 class PredictResource(Resource):
     def post(self):
-        # Get the image filename from the request payload
+        # Get the model name and image filename from the request payload
+        model_name = request.json['model']
         image_filename = request.json['imageFilename']
         
-        # Specify the path to your image file in the bucket
-        image_path = f'gs://{images_bucket_name}/{image_filename}'
-        
+        # Assign the appropriate model filename based on the requested model
+        if model_name == 'riceDisease':
+            model_filename = 'model-rice-diseases.h5'
+        elif model_name == 'nutrientDeficiency':
+            model_filename = 'model-nutrient-deficiency.h5'
+        elif model_name == 'riceVariety':
+            model_filename = 'model-rice-variety.h5'
+        else:
+            return jsonify({'error': 'Invalid model name'})
+
+        # Create a client for interacting with the GCS API
+        storage_client = storage.Client.from_service_account_json('./service-account-key.json')
+
+        # Create the directory if it doesn't exist
+        os.makedirs('/tmp/', exist_ok=True)
+
+         # Download the model file to a temporary location
+        temp_model_path = f'/tmp/{model_filename}'
+        model_blob = storage_client.get_bucket(bucket_name).blob(model_filename)
+        model_blob.download_to_filename(temp_model_path)
+
+        # Load the model from the H5 file
+        model = keras.models.load_model(temp_model_path, compile=False)
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
         # Download the image file to a temporary location
         temp_image_path = f'/tmp/{image_filename}'
         blob = storage_client.get_bucket(images_bucket_name).blob(image_filename)
@@ -57,12 +62,20 @@ class PredictResource(Resource):
         predicted_class = np.argmax(prediction)
     
         # Map the predicted class to the corresponding label
-        class_labels = ['Bacterial Leaf Blight', 'Brown Spot', 'Healthy', 'Leaf Blast', 'Leaf Scald', 'Narrow Brown Spot']
+        class_labels = {
+            'riceDisease': ['Bacterial Leaf Blight', 'Brown Spot', 'Healthy', 'Leaf Blast', 'Leaf Scald', 'Narrow Brown Spot'],
+            'nutrientDeficiency': ['Nitrogen', 'Phosphorus', 'Potassium'],
+            'riceVariety': ['Arborio', 'Basmati', 'Ipsala', 'Jasmine', 'Karacadag']
+        }
+        class_labels = class_labels.get(model_name)
+        if class_labels is None:
+            return jsonify({'error': 'Invalid model name'})
+        
         predicted_label = class_labels[predicted_class]
-    
+
         # Prepare the response
         response = {'prediction': predicted_label}
-    
+
         return jsonify(response)
 
 api.add_resource(PredictResource, '/')
